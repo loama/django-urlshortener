44de788df0950917845865744ee978f0c3a9c9f8
Jaime López <loama18@gmail.com>
2020-04-20 14:24:44 -0500

    add something

--- url/views.py
+++ url/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.shortcuts import redirect
+import random
+import string
 import short_url
 
 from url.models import shortened_url
@@ -9,8 +11,21 @@ from url.models import shortened_url
 from django.http import HttpResponse
 
 def index(request, urlToShorten):
-    return HttpResponse("Shortened " + urlToShorten)
+    # generate random code
+    letters = string.ascii_letters
+    id = ''.join(random.choice(letters) for i in range(3))
+
+    url = shortened_url(original_url=urlToShorten, shortened_url=id)
+    url.save()
+    return JsonResponse({
+        'status': 'success',
+        'data': {
+            'original_url': urlToShorten,
+            'shorten_url': id
+        }
+    })
 
 def redirect(request, urlToRedirect):
-    destination = shortened_url.objects.get(shortened_url='abc.co')
+    print(urlToRedirect)
+    destination = shortened_url.objects.get(shortened_url=urlToRedirect)
     return HttpResponseRedirect(str(destination))
